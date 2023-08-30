3ed14cbe6d0a4accf4389d2f7ea921626de25910
Ana Paula Leão <anaqueiroz.dev@gmail.com>
2023-08-30 08:37:54 -0300

    styles: summary, searchform components and table transactions

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,13 @@
-import logoImg from '../../assets/logo.svg';
-import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
+import logoImg from "../../assets/logo.svg";
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 export function Header() {
-   return (
-      <HeaderContainer>
-         <HeaderContent>
-            <img src={logoImg} alt='Logo DT Money' />
-            <NewTransactionButton>Nova Transação</NewTransactionButton>
-         </HeaderContent>
-      </HeaderContainer>
-   );
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="Logo DT Money" />
+        <NewTransactionButton>Nova Transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  );
 }
